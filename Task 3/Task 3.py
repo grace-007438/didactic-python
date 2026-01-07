35bd3ba592a6269429662c6059f5b4389c4a0103
grace-007438 <gracemariya366@gmail.com>
2026-01-07 16:14:27 +0530

    Create Task 3.py

---
+++ Task 3/Task 3.py
@@ -0,0 +1,11 @@
+marks = int(input("Enter your marks: "))
+if marks >= 40 and marks <= 50:
+    print("Grade: A")
+elif marks >= 30 and marks <40:
+    print("Grade: B")
+elif marks >= 25 and marks <30:
+    print("Grade: C")
+elif marks >= 15 and marks <25:
+    print("Grade: D")
+else:
+    print("Grade:Fail")

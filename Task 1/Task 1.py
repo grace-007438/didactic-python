8355d799b2355a4244a939cf2f4fb5e6bba18322
grace-007438 <gracemariya366@gmail.com>
2026-01-07 16:05:26 +0530

    Add user input and formatted output functionality

---
+++ Task 1/Task 1.py
@@ -0,0 +1,10 @@
+ # input user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+
+# \n formatted user
+print("\n formatted user")
+print(f"\nName     : {name}")
+print(f"\nAge      : {age}")
+print(f"\nLocation : {location}")
